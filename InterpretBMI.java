5520cfd9e4015c87c863156510e08735f7e42359
Chris <ctbernardabc@gmail.com>
2023-02-10 02:09:07 -0500

    Done.

--- InterpretBMI.java
+++ InterpretBMI.java
@@ -1,4 +1,37 @@
-package HWCh3;
+/**
+ * @class: HWCh3.InterpretBMI
+ * @author Chris Bernard
+ * @course ITEC 2140
+ * @version 1.0
+ * @date February 2
+ * description: this program will determine your bmi from your weight (in pounds) and your height (in inches).
+ */
+import java.util.Scanner;
 
 public class InterpretBMI {
+    public static void main(String[] args) {
+        double weight;
+        double height;
+        Scanner scanner = new Scanner(System.in);
+
+        double bmiValue;
+        System.out.println("[This is a BMI Calculator]");
+        System.out.print("Enter your weight in pounds: ");
+        weight = scanner.nextDouble() * .4536; //Entered in weight from pounds saved directly to kilograms
+        System.out.print("Enter your height in inches: ");
+        height = scanner.nextDouble() * .0254; //Entered in height from inches saved directly to meters
+
+        bmiValue = weight / (height * height);
+
+        if (bmiValue < 18.5) {
+            System.out.println("You are underweight");
+        } else if (18.5 <= bmiValue && bmiValue <= 25.0) {
+            System.out.println("You are normal weight");
+        } else if (25.0 <= bmiValue && bmiValue <= 30.0) {
+            System.out.println("You are overweight");
+        } else if (bmiValue >= 30.0) {
+            System.out.println("You are obese");
+        }
+    }
+
 }
--- PerimeterOfTriangle.java
+++ PerimeterOfTriangle.java
@@ -1,4 +1,35 @@
-package HWCh3;
+/**
+ * @class: HWCh3.PerimeterOfTriangle
+ * @author Chris Bernard
+ * @course ITEC 2140
+ * @version 1.0
+ * @date February 9
+ * description: this program will determine if a triangle is possible given the three sides and will return the perimeter if valid
+ */
+
+import java.util.Scanner;
 
 public class PerimeterOfTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[Triangle Perimeter Finding Calculator] Enter how long sides for your triangle are");
+        System.out.print("First side: ");
+
+        double firstSide = scanner.nextDouble();
+
+        System.out.print("Second side: ");
+
+        double secondSide = scanner.nextDouble();
+        System.out.print("Last side: ");
+
+        double lastSide = scanner.nextDouble();
+
+        if ((firstSide + secondSide) > lastSide && (firstSide + lastSide) > secondSide && (secondSide + lastSide) > firstSide) {
+            System.out.print("The perimeter of the triangle is: ");
+            System.out.println(firstSide + secondSide + lastSide);
+        } else {
+            System.out.println("invalid input");
+        }
+    }
 }
